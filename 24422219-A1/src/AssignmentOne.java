7fadbc57ca4a2db1e154ae7d2e394f688c810712
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-07-14 04:48:59 +1100

    Update AssignmentOne.java

--- 24422219-A1/src/AssignmentOne.java
+++ 24422219-A1/src/AssignmentOne.java
@@ -1,6 +1,14 @@
 public class AssignmentOne {
 
     public static class HealthProfessional {
+        private int ID;
+        private String name;
+        private String department;
 
+        public HealthProfessional(){
+            this.ID = 0;
+            this.name = "";
+            this.department= "";
+        }
     }
 }
