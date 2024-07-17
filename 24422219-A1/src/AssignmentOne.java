2c181a68e6eb56d8a98ac3fc3875b91f16738d30
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-07-18 00:17:55 +1100

    final

--- 24422219-A1/src/AssignmentOne.java
+++ 24422219-A1/src/AssignmentOne.java
@@ -1,26 +1,32 @@
+// Import the ArrayList class
 import java.util.ArrayList;
+// Import the List interface
 import java.util.List;
 
+// Part 1 base class as per the request
 public class AssignmentOne {
 
+    // Health Professional base class
     public static class HealthProfessional {
         private int ID;
         private String name;
         private String department;
 
+        // default constructor
         public HealthProfessional(){
             this.ID = 0;
             this.name = "default";
             this.department= "default";
         }
 
+        //  second constructor that initialises all the instance variables
         public HealthProfessional(int ID, String name, String department){
             this.ID = ID;
             this.name = name;
             this.department = department;
         }
 
-        // Getters and setters
+        // Getters and setters for validation inputs
         public int getID() {
             return ID;
         }
@@ -45,7 +51,7 @@ public class AssignmentOne {
             this.department = department;
         }
 
-
+        //method that print details
         public void printDetails() {
             System.out.println("ID: " + ID);
             System.out.println("Name: " + name);
@@ -55,8 +61,10 @@ public class AssignmentOne {
 
     }
 
-    // Part 2
+    // Part 2 - – Child classes
     public static class GeneralPractitioner extends HealthProfessional {
+
+        // Additional variable
         private String clinicName;
 
         // Default constructor
@@ -65,11 +73,22 @@ public class AssignmentOne {
             this.clinicName = "default Clinic";
         }
 
+        // Parameterized constructor
         public GeneralPractitioner(int ID, String name, String department, String clinicName) {
             super(ID, name, department);
             this.clinicName = clinicName;
         }
 
+        // Getter and setter
+        public String getClinicName() {
+            return clinicName;
+        }
+
+        public void setClinicName(String clinicName) {
+            this.clinicName = clinicName;
+        }
+
+        // Overriding printDetails method from the super class HealthProfessional
         @Override
         public void printDetails() {
             System.out.println("--The doctor details are:--");
@@ -79,9 +98,11 @@ public class AssignmentOne {
         }
     }
 
+    // Cardiologist class
     public static class Cardiologist extends HealthProfessional {
         private String hospitalName;
 
+        //default constructor
         public Cardiologist() {
             super();
             this.hospitalName = "default Hospital";
@@ -92,6 +113,7 @@ public class AssignmentOne {
             this.hospitalName = hospitalName;
         }
 
+        // Getters and setters
         public String getHospitalName() {
             return hospitalName;
         }
@@ -100,6 +122,7 @@ public class AssignmentOne {
             this.hospitalName = hospitalName;
         }
 
+        // Overriding printDetails method from the super class HealthProfessional
         @Override
         public void printDetails() {
             System.out.println("--The doctor details are:--");
@@ -126,7 +149,7 @@ public class AssignmentOne {
             this.selectedDoctor = null;
         }
 
-        // constructor
+        // constructor for appointments
         public Appointment(String patientName, String mobilePhone, String preferredTimeSlot, HealthProfessional selectedDoctor) {
             this.patientName = patientName;
             this.mobilePhone = mobilePhone;
@@ -184,6 +207,8 @@ public class AssignmentOne {
     }
 
     // Part 5 – Collection of appointments
+    // Also, make sure all required information is
+    //supplied when creating a new appointment. Otherwise, the appointment can not be created
     public static void createAppointment(List<Appointment> appointments, String patientName, String mobilePhone, String preferredTimeSlot, HealthProfessional doctor) {
         if (patientName != null && !patientName.isEmpty() && mobilePhone != null && !mobilePhone.isEmpty() && preferredTimeSlot != null && !preferredTimeSlot.isEmpty() && doctor != null) {
             Appointment appointment = new Appointment(patientName, mobilePhone, preferredTimeSlot, doctor);
@@ -232,9 +257,11 @@ public class AssignmentOne {
         GeneralPractitioner gp1 = new GeneralPractitioner(101, "Dr. Ashly Young", "General Medicine", "Clayton Clinic");
         GeneralPractitioner gp2 = new GeneralPractitioner(102, "Dr. Lisa Collet", "General Medicine", "Ashburton Clinic");
         GeneralPractitioner gp3 = new GeneralPractitioner(103, "Dr. Annie Mel", "Family Medicine", "Springvales Clinic");
+
         // objects of Cardiologists
         Cardiologist c1 = new Cardiologist(201, "Dr. Vini Anderson", "Cardiology", "Monash Heart Hospital");
         Cardiologist c2 = new Cardiologist(202, "Dr. Emily Ross", "Cardiology", "Kingston Hospital");
+
         // print health professionals
         gp1.printDetails();
         System.out.println("------------------------------");
@@ -248,17 +275,34 @@ public class AssignmentOne {
         c2.printDetails();
         System.out.println("------------------------------");
 
+
         // Part 5 – Collection of appointments
         // Declare an ArrayList to store objects Appointment class
         List<Appointment> appointments = new ArrayList<>();
 
         //Create new appointments
+
+        //test
+        createAppointment(appointments, "", "0451064234", "08:00", gp1);
+
         createAppointment(appointments, "Roshali Obris", "0451064234", "08:00", gp1);
+        createAppointment(appointments, "Norma Fernando", "0452333143", "10:00", gp2);
+
+        createAppointment(appointments, "Andrea Perera", "0456567413", "14:30", c1);
+        createAppointment(appointments, "Michael Ester", "0454535769", "16:00", c2);
+
 
         // Print appointments
         System.out.println("--Existing Appointments:--");
         printExistingAppointments(appointments);
 
-        
+        // Cancel one of the existing appointments
+        cancelBooking(appointments, "0452333143");
+
+        // Print appointments again
+        System.out.println("--Updated Appointments List:--");
+        printExistingAppointments(appointments);
+
+
     }
 }
